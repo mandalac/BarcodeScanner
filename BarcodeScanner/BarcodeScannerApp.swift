9320965dcebcf17eb400b590ffd454f528806a8f
Vasili Mandalac <mandalacvasili1991@gmail.com>
2021-02-15 10:42:48 -0600

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Vasili Mandalac on 2/15/21.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
